60724b0286fb663ae69eebea71b0fd32e7c4bdfd
Megan Karbowski <mck064@gmail.com>
2020-04-27 22:40:40 -0700

    figured out correct aspect ration for size of dvd cover

--- bong-joon-ho/src/app/app.component.spec.ts
+++ bong-joon-ho/src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatCardModule
+
       ],
       declarations: [
         AppComponent
