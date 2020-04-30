68acba417a664a12549d129fbf8ab2874266917a
Megan Karbowski <mck064@gmail.com>
2020-04-29 23:54:44 -0700

    more formatting, added preview overlay

--- bong-joon-ho/src/app/app.component.ts
+++ bong-joon-ho/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bong-joon-ho';
+  isTemplate: boolean;
+  constructor() {
+    this.isTemplate = true;
+  }
 }
+
