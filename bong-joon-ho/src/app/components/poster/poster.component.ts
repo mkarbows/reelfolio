f209170f9271274180fb08f1e32648ec9c70fa78
Megan Karbowski <mck064@gmail.com>
2020-05-13 00:29:19 -0700

    there are compile errors but it's bed time

--- bong-joon-ho/src/app/components/poster/poster.component.ts
+++ bong-joon-ho/src/app/components/poster/poster.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PosterService, Config } from 'src/app/services/poster.service';
+import { Movie } from 'src/assets/movie';
 
 @Component({
   selector: 'app-poster',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poster.component.css']
 })
 export class PosterComponent implements OnInit {
+  isTemplate: boolean;
+  config: Config;
+  movie1: Movie;
+  poster1: string;
 
-  constructor() { }
+  constructor(private posterService: PosterService) {
+    this.isTemplate = true;
+  }
 
   ngOnInit(): void {
   }
+
+  
 
 }
